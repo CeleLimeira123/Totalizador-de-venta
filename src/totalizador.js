c09ecf9e4a5c4fa5ccc49e28c7484e73c7e7ae82
Celeste Limeira <pandalimlim@gmail.com>
2026-09-08 07:16:44 -0400

    feat: agregar calculo de impuesto para CA y corregir imports de pruebas

--- src/totalizador.js
+++ src/totalizador.js
@@ -1,3 +1,10 @@
 export function calcularPrecioNeto(cantidad, precio) {
   return cantidad * precio;
 }
+
+export function calcularImpuesto(precioNeto, estado) {
+  if (estado === "CA") {
+    return Number((precioNeto * 0.0825).toFixed(2));
+  }
+  return 0;
+}
--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -1,7 +1,13 @@
-import { calcularPrecioNeto } from "./totalizador";
+import { calcularPrecioNeto, calcularImpuesto } from "./totalizador";
 
 describe("Totalizador de Ventas - Precio Neto", () => {
   it("deberia calcular el precio neto multiplicando cantidad por precio", () => {
     expect(calcularPrecioNeto(20, 3)).toEqual(60);
+  });
+});
+
+describe("Totalizador de Ventas - Impuestos", () => {
+  it("deberia calcular el impuesto de 8.25% para CA", () => {
+    expect(calcularImpuesto(60, "CA")).toEqual(4.95);
   });
 });
