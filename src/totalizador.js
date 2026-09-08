a27031bdbf26af6e0f6d550f3af6cd140b2b22ff
Celeste Limeira <pandalimlim@gmail.com>
2026-09-08 07:23:49 -0400

    feat: agregar calculo de impuesto de 4.00% para AL

--- src/totalizador.js
+++ src/totalizador.js
@@ -6,5 +6,8 @@ export function calcularImpuesto(precioNeto, estado) {
   if (estado === "CA") {
     return Number((precioNeto * 0.0825).toFixed(2));
   }
+  if (estado === "AL") {
+    return Number((precioNeto * 0.0400).toFixed(2));
+  }
   return 0;
 }
--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -10,4 +10,7 @@ describe("Totalizador de Ventas - Impuestos", () => {
   it("deberia calcular el impuesto de 8.25% para CA", () => {
     expect(calcularImpuesto(60, "CA")).toEqual(4.95);
   });
+  it("deberia calcular el impuesto de 4.00% para AL", () => {
+  expect(calcularImpuesto(60, "AL")).toEqual(2.40);
+});
 });
